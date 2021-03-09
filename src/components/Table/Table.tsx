1dfdac4366553d275dadd712694f3c007c83c968
Kyle Bonar <kylewb2011@gmail.com>
2021-03-09 17:21:55 -0600

    Tinkering with styles

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,12 +1,29 @@
 import * as React from "react";
 import DataTable from "react-data-table-component";
+import styled from "styled-components";
 import { data } from "./data";
 import { columns } from "./columns";
+
+const StyledTable = styled(DataTable)`
+  div[role="columnheader"] {
+    &:hover,
+    &:focus {
+      color: inherit;
+    }
+  }
+
+  div.rdt_TableRow {
+    &:hover,
+    &:focus {
+      background-color: #eee;
+    }
+  }
+`;
 
 const Table: React.FC = () => {
   return (
     <>
-      <DataTable title="Movie List" columns={columns} data={data} />
+      <StyledTable title="Movie List" columns={columns} data={data} />
     </>
   );
 };
