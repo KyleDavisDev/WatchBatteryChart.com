80537a62ff8edca72edaf274b0e2093c1c1c8a3a
Kyle Bonar <kylewb2011@gmail.com>
2021-03-09 17:11:35 -0600

    init commit

---
+++ src/components/Table/columns.tsx
@@ -0,0 +1,44 @@
+import { IDataTableColumn } from "react-data-table-component";
+import * as React from "react";
+
+interface IColumn extends IDataTableColumn {}
+
+export const columns: IColumn[] = [
+  {
+    name: (
+      <span>
+        Energizer
+        <br /> Eveready
+        <br /> Rayovac
+      </span>
+    ),
+    selector: "Energizer",
+    sortable: true,
+    compact: false
+  },
+  {
+    name: (
+      <span>
+        Maxell
+        <br />
+        National
+        <br />
+        Panasonic
+        <br />
+        Sony
+        <br />
+        Toshiba
+      </span>
+    ),
+    selector: "Maxell",
+    sortable: true
+  },
+
+  { name: "Timex", selector: "Timex", sortable: true },
+  { name: "Duracell", selector: "Duracell", sortable: true },
+  {
+    name: "Dimensions (MM)",
+    selector: "Dimensions(mm)",
+    sortable: true
+  }
+];
---
+++ src/components/Table/data.tsx
@@ -0,0 +1,402 @@
+interface IData {
+  Energizer?: string;
+  Maxell?: string;
+  Duracell?: string;
+  Timex?: string;
+  "Dimensions(mm)"?: string;
+}
+
+export const data: IData[] = [
+  {
+    Energizer: "301",
+    Maxell: "SR43SW",
+    Duracell: "D301",
+    Timex: "D",
+    "Dimensions(mm)": "11.6 x 4.2"
+  },
+  {
+    Energizer: "303",
+    Maxell: "SR44SW",
+    Duracell: "D303",
+    Timex: "A",
+    "Dimensions(mm)": "11.6 x 5.4"
+  },
+  {
+    Energizer: "309",
+    Maxell: "SR754SW",
+    Duracell: "D309",
+    Timex: "--",
+    "Dimensions(mm)": "7.9 x 5.35"
+  },
+  {
+    Energizer: "313",
+    Duracell: "D313",
+    Maxell: "--",
+    Timex: "--",
+    "Dimensions(mm)": "11.6 x 5.4"
+  },
+  {
+    Energizer: "314",
+    Duracell: "--",
+    Maxell: "SR716W",
+    Timex: "--",
+    "Dimensions(mm)": "7.9 x 1.68"
+  },
+  {
+    Energizer: "315",
+    Duracell: "D315",
+    Maxell: "SR716SW",
+    Timex: "HA",
+    "Dimensions(mm)": "7.9 x 1.65"
+  },
+  {
+    Energizer: "317",
+    Duracell: "D317",
+    Maxell: "SR516SW",
+    Timex: "CA",
+    "Dimensions(mm)": "5.8 x 1.65"
+  },
+  {
+    Energizer: "319",
+    Duracell: "D319",
+    Maxell: "SR527SW",
+    Timex: "--",
+    "Dimensions(mm)": "5.8 x 2.6"
+  },
+  {
+    Energizer: "321",
+    Duracell: "D321",
+    Maxell: "SR616SW",
+    Timex: "DA",
+    "Dimensions(mm)": "6.8 x 1.65"
+  },
+  {
+    Energizer: "323",
+    Duracell: "D323",
+    Maxell: "--",
+    Timex: "--",
+    "Dimensions(mm)": "7.9 x 5.4"
+  },
+  {
+    Energizer: "325",
+    Duracell: "D325",
+    Maxell: "--",
+    Timex: "--",
+    "Dimensions(mm)": "7.9 x 3.6"
+  },
+  {
+    Energizer: "328",
+    Duracell: "--",
+    Maxell: "--",
+    Timex: "--",
+    "Dimensions(mm)": "--"
+  },
+  {
+    Energizer: "329",
+    Duracell: "D329",
+    Maxell: "SR731W",
+    Timex: "--",
+    "Dimensions(mm)": "7.9 x 3.1"
+  },
+  {
+    Energizer: "333",
+    Duracell: "--",
+    Maxell: "SR610SW",
+    Timex: "--",
+    "Dimensions(mm)": "6.8 x 1.1"
+  },
+  {
+    Energizer: "335",
+    Duracell: "--",
+    Maxell: "SR512SW",
+    Timex: "--",
+    "Dimensions(mm)": "5.8 x 1.25"
+  },
+  {
+    Energizer: "337",
+    Duracell: "--",
+    Maxell: "SR416SW",
+    Timex: "--",
+    "Dimensions(mm)": "4.8 x 1.65"
+  },
+  {
+    Energizer: "339",
+    Duracell: "--",
+    Maxell: "SR",
+    Timex: "--",
+    "Dimensions(mm)": "--"
+  },
+  {
+    Energizer: "341",
+    Duracell: "--",
+    Maxell: "SR",
+    Timex: "--",
+    "Dimensions(mm)": "--"
+  },
+  {
+    Energizer: "343",
+    Duracell: "D343",
+    Maxell: "--",
+    Timex: "--",
+    "Dimensions(mm)": "11.6 x 3.6"
+  },
+  {
+    Energizer: "344",
+    Duracell: "D344",
+    Maxell: "SR1136SW",
+    Timex: "--",
+    "Dimensions(mm)": "11.6 x 3.6"
+  },
+  {
+    Energizer: "346",
+    Duracell: "--",
+    Maxell: "SR712SW",
+    Timex: "--",
+    "Dimensions(mm)": "7.9 x 1.3"
+  },
+  {
+    Energizer: "348",
+    Duracell: "--",
+    Maxell: "SR421SW",
+    Timex: "--",
+    "Dimensions(mm)": "4.8 x 2.15"
+  },
+  {
+    Energizer: "350",
+    Duracell: "D350",
+    Maxell: "--",
+    Timex: "--",
+    "Dimensions(mm)": "11.6 x 3.6"
+  },
+  {
+    Energizer: "354",
+    Duracell: "D354",
+    Maxell: "--",
+    Timex: "--",
+    "Dimensions(mm)": "11.6 x 4.2"
+  },
+  {
+    Energizer: "355",
+    Duracell: "--",
+    Maxell: "--",
+    Timex: "--",
+    "Dimensions(mm)": "15.2 x 4.8"
+  },
+  {
+    Energizer: "357",
+    Duracell: "D357",
+    Maxell: "SR44W",
+    Timex: "J",
+    "Dimensions(mm)": "11.6 x 5.4"
+  },
+  {
+    Energizer: "361",
+    Duracell: "D361",
+    Maxell: "SR721W",
+    Timex: "X",
+    "Dimensions(mm)": "7.9 x 2.1"
+  },
+  {
+    Energizer: "362",
+    Duracell: "D362",
+    Maxell: "SR721SW",
+    Timex: "S",
+    "Dimensions(mm)": "7.9 x 2.1"
+  },
+  {
+    Energizer: "364",
+    Duracell: "D364",
+    Maxell: "SR621SW",
+    Timex: "T",
+    "Dimensions(mm)": "6.8 x 2.15"
+  },
+  {
+    Energizer: "365",
+    Duracell: "--",
+    Maxell: "SR1116W",
+    Timex: "--",
+    "Dimensions(mm)": "11.6 x 1.15"
+  },
+  {
+    Energizer: "366",
+    Duracell: "D366",
+    Maxell: "SR1116SW",
+    Timex: "--",
+    "Dimensions(mm)": "11.6 x 1.65"
+  },
+  {
+    Energizer: "370",
+    Duracell: "D370",
+    Maxell: "SR920W",
+    Timex: "Z",
+    "Dimensions(mm)": "9.5 x 2.1"
+  },
+  {
+    Energizer: "371",
+    Duracell: "D371",
+    Maxell: "SR920SW",
+    Timex: "--",
+    "Dimensions(mm)": "9.5 x 2.1"
+  },
+  {
+    Energizer: "372",
+    Duracell: "D372",
+    Maxell: "SR916SW",
+    Timex: "WA",
+    "Dimensions(mm)": "9.5 x 1.65"
+  },
+  {
+    Energizer: "373",
+    Duracell: "D373",
+    Maxell: "SR916SW",
+    Timex: "WA",
+    "Dimensions(mm)": "9.5 x 1.65"
+  },
+  {
+    Energizer: "376",
+    Duracell: "--",
+    Maxell: "SR626W",
+    Timex: "MA",
+    "Dimensions(mm)": "6.8 x 2.6"
+  },
+  {
+    Energizer: "377",
+    Duracell: "D377",
+    Maxell: "SR626SW",
+    Timex: "BA",
+    "Dimensions(mm)": "6.8 x 2.6"
+  },
+  {
+    Energizer: "379",
+    Duracell: "D379",
+    Maxell: "SR521SW",
+    Timex: "JA",
+    "Dimensions(mm)": "5.8 x 2.15"
+  },
+  {
+    Energizer: "380",
+    Duracell: "D380",
+    Maxell: "SR936W",
+    Timex: "--",
+    "Dimensions(mm)": "9.5 x 3.6"
+  },
+  {
+    Energizer: "381",
+    Duracell: "--",
+    Maxell: "SR1120SW",
+    Timex: "--",
+    "Dimensions(mm)": "11.6 x 2.05"
+  },
+  {
+    Energizer: "384",
+    Duracell: "D384",
+    Maxell: "SR41SW",
+    Timex: "--",
+    "Dimensions(mm)": "7.9 x 3.6"
+  },
+  {
+    Energizer: "386",
+    Duracell: "D386",
+    Maxell: "SR43W",
+    Timex: "H",
+    "Dimensions(mm)": "11.6 x 4.2"
+  },
+  {
+    Energizer: "387",
+    Duracell: "D387",
+    Maxell: "--",
+    Timex: "--",
+    "Dimensions(mm)": "11.6 x 3.3"
+  },
+  {
+    Energizer: "388",
+    Duracell: "D388",
+    Maxell: "--",
+    Timex: "--",
+    "Dimensions(mm)": "8.8 x 5.3"
+  },
+  {
+    Energizer: "389",
+    Duracell: "D389",
+    Maxell: "SR1130W",
+    Timex: "M",
+    "Dimensions(mm)": "11.6 x 3.0"
+  },
+  {
+    Energizer: "390",
+    Duracell: "D390",
+    Maxell: "SR1130SW",
+    Timex: "--",
+    "Dimensions(mm)": "11.6 x 3.0"
+  },
+  {
+    Energizer: "391",
+    Duracell: "D391",
+    Maxell: "SR1120W",
+    Timex: "L",
+    "Dimensions(mm)": "11.6 x 2.05"
+  },
+  {
+    Energizer: "392",
+    Duracell: "D392",
+    Maxell: "SR41W",
+    Timex: "K",
+    "Dimensions(mm)": "7.9 x 3.6"
+  },
+  {
+    Energizer: "393",
+    Duracell: "D393",
+    Maxell: "SR754W",
+    Timex: "F",
+    "Dimensions(mm)": "7.9 x 5.35"
+  },
+  {
+    Energizer: "394",
+    Duracell: "D394",
+    Maxell: "SR936SW",
+    Timex: "--",
+    "Dimensions(mm)": "9.5 x 3.6"
+  },
+  {
+    Energizer: "395",
+    Duracell: "D395",
+    Maxell: "SR927W",
+    Timex: "--",
+    "Dimensions(mm)": "9.5 x 2.6"
+  },
+  {
+    Energizer: "396",
+    Duracell: "D396",
+    Maxell: "SR726SW",
+    Timex: "--",
+    "Dimensions(mm)": "7.9 x 2.6"
+  },
+  {
+    Energizer: "397",
+    Duracell: "D397",
+    Maxell: "SR726SW",
+    Timex: "--",
+    "Dimensions(mm)": "9.5 x 3.6"
+  },
+  {
+    Energizer: "399",
+    Duracell: "D399",
+    Maxell: "SR927SW",
+    Timex: "--",
+    "Dimensions(mm)": "9.5 x 2.6"
+  },
+  {
+    Energizer: "750",
+    Duracell: "--",
+    Maxell: "--",
+    Timex: "--",
+    "Dimensions(mm)": "5.7 x 1.0"
+  },
+  {
+    Energizer: "751",
+    Duracell: "--",
+    Maxell: "--",
+    Timex: "--",
+    "Dimensions(mm)": "5.8 x 1.2"
+  }
+];
