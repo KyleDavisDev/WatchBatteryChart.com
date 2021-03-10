c0892cbd17352ad781b1d1b0c6ac9a34a2c5834d
Kyle Bonar <kylewb2011@gmail.com>
2021-03-09 21:57:29 -0600

    Filter rows by text input

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import DataTable from "react-data-table-component";
 import styled from "styled-components";
-import { data } from "./data";
+import { data, IData } from "./data";
 import { columns } from "./columns";
+import FilterInput from "./components/FilterInput";
 
 const StyledTable = styled(DataTable)`
   div[role="columnheader"] {
@@ -12,7 +13,7 @@ const StyledTable = styled(DataTable)`
     }
   }
 
-  div.rdt_TableRow {
+  div.rdt_TableRow[role="row"] {
     &:hover,
     &:focus {
       background-color: #eee;
@@ -21,9 +22,47 @@ const StyledTable = styled(DataTable)`
 `;
 
 const Table: React.FC = () => {
+  const [filterText, setFilterText] = React.useState("");
+  const [resetPaginationToggle, setResetPaginationToggle] = React.useState(
+    false
+  );
+
+  const subHeaderComponentMemo = React.useMemo(() => {
+    const handleClear = () => {
+      if (filterText) {
+        setResetPaginationToggle(!resetPaginationToggle);
+        setFilterText("");
+      }
+    };
+
+    return (
+      <FilterInput
+        onChange={e => setFilterText(e.target.value)}
+        onClear={handleClear}
+        filterText={filterText}
+      />
+    );
+  }, [filterText, resetPaginationToggle]);
+
+  const filteredData = data.filter((row: IData) => {
+    if (filterText.length === 0) return true;
+
+    return Object.keys(row).find(column => {
+      if (row[column].indexOf(filterText) > -1) return true;
+      return false;
+    });
+  });
+
   return (
     <>
-      <StyledTable title="Movie List" columns={columns} data={data} />
+      <StyledTable
+        title="Movie List"
+        columns={columns}
+        data={filteredData}
+        fixedHeader={true}
+        subHeader={true}
+        subHeaderComponent={subHeaderComponentMemo}
+      />
     </>
   );
 };
