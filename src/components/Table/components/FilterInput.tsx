2cfddbacb5339754a545c0eacd35ac2beb6e3214
Kyle Bonar <kylewb2011@gmail.com>
2021-03-10 23:15:50 -0600

    styling table

--- src/components/Table/components/FilterInput.tsx
+++ src/components/Table/components/FilterInput.tsx
@@ -1,47 +1,41 @@
 import * as React from "react";
 import styled from "styled-components";
+import { MouseEventHandler } from "react";
 
-const TextField = styled.input`
-  height: 32px;
-  width: 200px;
-  border-radius: 3px;
-  border-top-left-radius: 5px;
-  border-bottom-left-radius: 5px;
-  border-top-right-radius: 0;
-  border-bottom-right-radius: 0;
-  border: 1px solid #e5e5e5;
-  padding: 0 32px 0 16px;
-
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-const ClearButton = styled.button`
-  border-top-left-radius: 0;
-  border-bottom-left-radius: 0;
-  border-top-right-radius: 5px;
-  border-bottom-right-radius: 5px;
-  height: 34px;
-  width: 32px;
-  text-align: center;
+const StyledContainer = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: center;
+  justify-content: flex-end;
+  align-items: stretch;
+
+  input {
+    width: 200px;
+    border-radius: 5px 0 0 5px;
+    border: 1px solid #e5e5e5;
+    padding: 0 32px 0 16px;
+  }
+
+  button {
+    border-radius: 0 5px 5px 0;
+    padding: 0 20px;
+    text-align: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 `;
 
 export interface IFilterInput {
   filterText?: string;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  onClear: (event: React.ChangeEvent<HTMLButtonElement>) => void;
+  onClear: MouseEventHandler<HTMLButtonElement>;
 }
 
 const FilterInput: React.FC<IFilterInput> = props => {
   const { filterText, onChange, onClear } = props;
 
   return (
-    <>
-      <TextField
+    <StyledContainer>
+      <input
         id="search"
         type="text"
         placeholder="Filter By Name"
@@ -49,10 +43,10 @@ const FilterInput: React.FC<IFilterInput> = props => {
         value={filterText}
         onChange={onChange}
       />
-      <ClearButton type="button" onClick={onClear}>
+      <button type="button" onClick={onClear}>
         X
-      </ClearButton>
-    </>
+      </button>
+    </StyledContainer>
   );
 };
 
